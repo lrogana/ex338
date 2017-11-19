897327a7115c078566eef6e33137c47f6a7eed59
Axel Clark <axelclark2@yahoo.com>
2017-11-19 16:53:26 -0500

    formate trade controller

--- lib/ex338_web/controllers/trade_controller.ex
+++ lib/ex338_web/controllers/trade_controller.ex
@@ -16,9 +16,11 @@ defmodule Ex338Web.TradeController do
   def index(conn, %{"fantasy_league_id" => league_id}) do
     league = FantasyLeague.Store.get(league_id)
 
-    render(conn, "index.html",
-     fantasy_league: league,
-     trades: Trade.Store.all_for_league(league.id)
+    render(
+      conn,
+      "index.html",
+      fantasy_league: league,
+      trades: Trade.Store.all_for_league(league.id)
     )
   end
 
@@ -29,7 +31,9 @@ defmodule Ex338Web.TradeController do
     league_teams = FantasyTeam.Store.list_teams_for_league(league_id)
     league_players = FantasyTeam.Store.owned_players_for_league(league_id)
 
-    render(conn, "new.html",
+    render(
+      conn,
+      "new.html",
       changeset: changeset,
       fantasy_team: team,
       league_teams: league_teams,
@@ -48,12 +52,14 @@ defmodule Ex338Web.TradeController do
         league_teams = FantasyTeam.Store.list_teams_for_league(league_id)
         league_players = FantasyTeam.Store.owned_players_for_league(league_id)
 
-        render(conn, "new.html",
-         changeset: changeset,
-         fantasy_team: team,
-         league_teams: league_teams,
-         league_players: league_players,
-       )
+        render(
+          conn,
+          "new.html",
+          changeset: changeset,
+          fantasy_team: team,
+          league_teams: league_teams,
+          league_players: league_players,
+        )
     end
   end
 
